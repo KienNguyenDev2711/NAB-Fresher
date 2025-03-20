4e99554db95332fa833e050fe7436a1aa738985e
KienNguyenDev2711 <kiennguyenfpt2711@gmail.com>
2025-03-20 22:35:32 +0700

    change comment to english

--- src/main/java/org/kiennguyenfpt/nabfresher/task2/SolutionTaskTwo.java
+++ src/main/java/org/kiennguyenfpt/nabfresher/task2/SolutionTaskTwo.java
@@ -2,13 +2,13 @@ package org.kiennguyenfpt.nabfresher.task2;
 
 public class SolutionTaskTwo {
     public int solution(int[] D, int X) {
-        int days = 1; // Bắt đầu từ ngày đầu tiên
-        int start = 0; // Nhiệm vụ bắt đầu trong ngày
+        int days = 1; // start from the first day
+        int start = 0; // start from the first task
 
         for (int i = 1; i < D.length; i++) {
             if (D[i] - D[start] > X) {
-                days++; // Tăng số ngày
-                start = i; // Bắt đầu ngày mới từ nhiệm vụ hiện tại
+                days++; // increase the number of days
+                start = i; // start from the next task
             }
         }
 
